2106ebe66c52db70a3c7c0b3c5be53446a5330cb
Jaja Brown <65190496+jbrown827@users.noreply.github.com>
2021-02-04 22:00:51 -0600

    Initial Commit and setup

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name)
+VALUES ("The Uptown");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Cheddar Beast");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Peanut Butter Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Half and Half");
