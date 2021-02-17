ae625145d3c2ce6adbf48d757f8425ffcc7aee12
Jaja Brown <65190496+jbrown827@users.noreply.github.com>
2021-02-17 00:24:17 -0600

    Connect jawsDB

--- config/connection.js
+++ config/connection.js
@@ -8,12 +8,18 @@ const connection = mysql.createConnection({
   database: "burgers_db",
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.log("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+//
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "burgers_db",
+  });
+}
+
+connection.connect();
 
 module.exports = connection;
